fa362a927e9c4f8adfc85fc06ecb6cd39795e039
ElsaDevOps <inatelsa@mail.example.com>
2025-10-03 13:19:38 +0100

    add challenge

--- flask_redis_projects/app.py
+++ flask_redis_projects/app.py
@@ -1,23 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import redis
+import os
 
 
 
+# Flask & Redis
 
 app = Flask(__name__)
+redis_host = os.getenv('REDIS_HOST', 'redis')
+redis_port = int(os.getenv('REDIS_PORT', 6379))
 
-@app.route('/')
-def hello_world():
-    
-# Connect to the Redis database
-    redis_client = redis.StrictRedis(host='redis', port=6379, decode_responses=True)
+# connect to redis (global variable)
+r = redis.Redis(host=redis_host, port=redis_port, db=0)
+
 
 @app.route('/')
 def home():
-    # Increment a counter in Redis
-    count = redis_client.incr('hits')
-    return jsonify(message="Welcome to the Flask-Redis app!", hits=count)
-  
+    return render_template("home.html")
+
+
+@app.route('/count')
+def count():
+    visits = r.incr('visit_count')
+    return render_template("count.html", visits=visits)
+
+                                  
+
+
+    
+
 
 if  __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
